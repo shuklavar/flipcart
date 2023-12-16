1e4c78c5f8027e4cc0a9f69b32dd1d880243665b
shuklavar <shuklavarunakar@gmail.com>
2023-12-16 20:36:40 +0530

    thirdcommi

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import Recat from './react'
+
+function Home() {
+    const [count, setCount] = useState(0)
+  
+    return (
+      <>
+       <h1>Software Engineer</h1>
+      </>
+    )
+  }
+  
+  export default Home
+  
